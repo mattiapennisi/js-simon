5671af851b03c06c7bf24a6ccc3f826aeb99ce18
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-20 20:22:45 +0100

    add script for checking result and for result output

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 // Variabili
 
 const countdown = document.querySelector('#countdown')
+const instructions = document.querySelector('#instructions')
 const numbersList = document.querySelector('#numbers-list')
 const listItem = document.querySelector('.listItem')
 const answersForm = document.querySelector('#answers-form')
+const inputBtn = document.querySelector('#inputBtn')
+const formControl = document.querySelector('.form-control')
+const inputOne = document.querySelector('#inputOne')
+const inputTwo = document.querySelector('#inputTwo')
+const inputThree = document.querySelector('#inputThree')
+const inputFour = document.querySelector('#inputFour')
+const inputFive = document.querySelector('#inputFive')
 
 let countdownCounter = 30
 let numbersArray = []
+let inputsArray = []
 
 // Fa partire il countdown
 
@@ -21,6 +30,7 @@ setInterval(function () {
         countdown.innerText = countdownCounter
         numbersList.classList.add('d-none')
         answersForm.classList.remove('d-none')
+        instructions.classList.add('d-none')
     }
 }, 100)
 
@@ -37,7 +47,68 @@ for (let i = 1; i <= 5; i++) {
     } else {
         i -= 1
     }
-}
+} 
 
 // Al click del button controlla se i valori inseriti dall'utente nel form corrispondono a quelli generati casualmente e fornisce un feedback
 
+inputBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let correctCheck = 0
+
+    if (numbersArray.includes(Number(inputOne.value)) && inputsArray.length < 5) {
+        inputsArray.push(inputOne.value)
+        
+        inputOne.classList.add('border-success')
+
+        correctCheck += 1
+    } else {
+        inputOne.classList.add('border-danger')
+    }
+
+    if (numbersArray.includes(Number(inputTwo.value)) && inputsArray.length < 5) {
+        inputsArray.push(inputTwo.value)
+        
+        inputTwo.classList.add('border-success')
+
+        correctCheck += 1
+    } else {
+        inputTwo.classList.add('border-danger')
+    }
+
+    if (numbersArray.includes(Number(inputThree.value)) && inputsArray.length < 5) {
+        inputsArray.push(inputThree.value)
+        
+        inputThree.classList.add('border-success')
+
+        correctCheck += 1
+    } else {
+        inputThree.classList.add('border-danger')
+    }
+
+    if (numbersArray.includes(Number(inputFour.value)) && inputsArray.length < 5) {
+        inputsArray.push(inputFour.value)
+        
+        inputFour.classList.add('border-success')
+
+        correctCheck += 1
+    } else {
+        inputFour.classList.add('border-danger')
+    }
+
+    if (numbersArray.includes(Number(inputFive.value)) && inputsArray.length < 5) {
+        inputsArray.push(inputFive.value)
+        
+        inputFive.classList.add('border-success')
+
+        correctCheck += 1
+    } else {
+        inputFive.classList.add('border-danger')
+    }
+
+    inputBtn.disabled = true
+
+    countdownCounter = ' '
+
+    instructions.innerText = `Hai indovinato ${correctCheck} numeri`
+})
